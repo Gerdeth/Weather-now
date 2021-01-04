a6d51a498925b1508beb96a2824faf94a3aa193e
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2021-01-04 07:51:50 -0500

    Link API to geolocation

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 window.addEventListener('load', ()=>{
-    let long;
+    let lon;
     let lat;
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
-            long=position.coords.longitude;
+            lon=position.coords.longitude;
             lat=position.coords.latitude;
-        })
-    }
-    fetch("https://api.weatherbit.io/v2.0/current?lat=23&lon=-30&key=dd9cf3aa3b8d4051bcb9057f6e286ad8")
-
+       
+    
+    const api =`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=dd9cf3aa3b8d4051bcb9057f6e286ad8`
+    fetch(api)
     .then(response => {
-        console.log(response);
+        return response.json();
     })
-    .catch(err => {
-        console.error(err);
+    .then(data => {
+        console.log(data);
     });
+    })}   
 });
 const temp = document.getElementsByClassName("temperature-degree")
