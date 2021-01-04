bacc0a8ba5eb64f3c35e2d6b1629978008a2f69b
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2021-01-04 17:39:14 -0500

    working app

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', ()=>{
     let lon;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let cityName = document.querySelector(".location-city");
+    let weatherIcon= document.querySelector(".icon");
+    
+
+    
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
@@ -8,14 +15,29 @@ window.addEventListener('load', ()=>{
             lat=position.coords.latitude;
        
     
-    const api =`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=dd9cf3aa3b8d4051bcb9057f6e286ad8`
-    fetch(api)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    });
-    })}   
+        const api =`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=dd9cf3aa3b8d4051bcb9057f6e286ad8`
+        fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                temperatureDegree.textContent = data.data[0].temp;
+                cityName.textContent= data.data[0].city_name;
+                temperatureDescription.textContent= data.data[0].weather.description;
+                // weatherIcon.textContent=data.data[0].weather.icon;
+            //    let iconAnimation = data.data[0].weather.icon;
+            //     setIcons(iconAnimation, document.querySelector('.icon'))
+
+            
+            });
+        });
+}
+// function setIcons(icon, iconID){
+//     // const iconAnimation = data.data[0].weather.icon;
+//     const skycons= new Skycons ({color:"white"});
+//     const currentIcon= iconAnimation.toUpperCase();
+//     skycons.play();
+//     return skycons.set(iconId, Skycons[currentIcon]);
+// }
 });
-const temp = document.getElementsByClassName("temperature-degree")
